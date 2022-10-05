4eef248ec91bd5c9c1ad82e6ae9412497fbc8e81
LAPTOP-30CLG7SB\PRASHANTH <prashanth11391@gmail.com>
2022-10-05 21:37:18 +0530

    DB connection change

--- db.js
+++ db.js
@@ -23,16 +23,27 @@ const mysql = require('mysql');
 //     console.log('Msql Connected')
 // });
 
+// const { Client } = require('pg')
+// const client = new Client({
+//     host: 'ec2-44-206-137-96.compute-1.amazonaws.com',
+//     user: 'edbexklhitvvon',
+//     password: '50a7c390d20e43dbd4f09f400ad7c5b99ba313ff64b9fb8f9c6fd0f5ecb1649b',
+//     database: 'd2ubecfm5nrdq7',
+//     port: 5432,
+//     ssl: {
+//         rejectUnauthorized: false
+//       }
+//   })
+//   client.connect()
+
+// pg -> postgres
 const { Client } = require('pg')
 const client = new Client({
-    host: 'ec2-44-206-137-96.compute-1.amazonaws.com',
-    user: 'edbexklhitvvon',
-    password: '50a7c390d20e43dbd4f09f400ad7c5b99ba313ff64b9fb8f9c6fd0f5ecb1649b',
-    database: 'd2ubecfm5nrdq7',
-    port: 5432,
-    ssl: {
-        rejectUnauthorized: false
-      }
+    host: 'dpg-ccupqc53t398cofejuv0-a',
+    user: 'studentdb_user',
+    password: 'WWLirAi60J2vScwMyBPtJPqfoaP33o0U',
+    database: 'studentdb',
+    port: 5432
   })
   client.connect()
 
